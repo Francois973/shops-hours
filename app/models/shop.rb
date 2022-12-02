01e3d47b35f8755e319f15740f8f10c1d88ae7e7
Francois973 <robino.francois973@gmail.com>
2022-12-02 14:24:58 +0100

    chore: display hours of shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,10 @@ class Shop < ApplicationRecord
   has_many :shop_hours, dependent: :destroy
 
   validates :name, presence: true
+
+  def monday
+    # @select_day ||= shop_hours.select { |s| s.day_of_week == 'monday' }
+    @select_day ||= shop_hours.where(day_of_week: 'monday')
+    @monday ||= @select_day.map(&:day_of_week).join
+  end
 end
