040490a17c836abe4effa28268d172aec904f1e4
Francois973 <robino.francois973@gmail.com>
2022-12-02 17:36:51 +0100

    remove validate method for order list shop hour

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,12 +2,4 @@ class Shop < ApplicationRecord
   has_many :shop_hours, dependent: :destroy
 
   validates :name, presence: true
-
-  validate :current_day_to
-
-  private
-
-  def current_day_to
-    shop_hours.order('day_of_week = ?')
-  end
 end
