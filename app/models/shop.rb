e26f5a12f835ede865b232a05c7d60c0438693ec
Francois973 <robino.francois973@gmail.com>
2022-12-02 17:16:25 +0100

    chore: add bold in hour's day

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,9 +3,11 @@ class Shop < ApplicationRecord
 
   validates :name, presence: true
 
-  def monday
-    # @select_day ||= shop_hours.select { |s| s.day_of_week == 'monday' }
-    @select_day ||= shop_hours.where(day_of_week: 'monday')
-    @monday ||= @select_day.map(&:day_of_week).join
+  validate :sort_with_day_today_first
+
+  private
+
+  def sort_with_day_today_first
+    shop_hours.sort_by { |day| day.day_of_week == Date.today.strftime('%A') }
   end
 end
