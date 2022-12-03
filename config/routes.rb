df3087202a34b9c67665ffc17369628fade72009
Francois973 <robino.francois973@gmail.com>
2022-12-03 20:54:42 +0100

    chore: add partiel shops in shared to render index of shop

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'shops#index'
 
+  # root 'shops#index'
   resources :shops, only: %i[index]
 end
